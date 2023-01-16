dc612744a11b5f630d48c6d4fa3b19a9ce871f39
keep <flymayday112@gmail.com>
2023-01-16 10:36:12 +0800

    dont forget array then return

--- daily5.js
+++ daily5.js
@@ -1,5 +1,5 @@
 let arr = [1, 2, 3, 4]
-let newArr = arr.map((item) => { return item * 2; });
+let newArrTest = arr.map((item) => { return item * 2; });
 // 這個就是undefined的
 let newArr0 = arr.map((item) => {});
 let newArr1 = arr.map((item) => { return });
@@ -7,7 +7,35 @@ let newArr1 = arr.map((item) => { return });
 
 let newArr2 = arr.map((item) => { return item * 3; });
 console.log(arr);
-console.log(newArr);
+console.log(newArrTest);
 console.log(newArr0);
 console.log(newArr1);
 console.log(newArr2);
+
+const array = [{
+        name: '小明',
+        age: 18
+    },
+    {
+        name: '小美',
+        age: 15
+    },
+    {
+        name: '杰倫',
+        age: 19
+    },
+    {
+        name: '漂亮阿姨',
+        age: 22
+    },
+    {
+        name: '老媽',
+        age: 28
+    }
+];
+let newArray = [];
+newArray = array.map((item) => { return `${item.name}${item.age}歲羅` });
+console.log(newArray);
+
+// 這樣寫就錯了..過程加總應放在return
+//console.log(newArray[].name+newArray[].age+`羅`);
